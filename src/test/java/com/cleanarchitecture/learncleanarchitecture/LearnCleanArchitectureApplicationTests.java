93cdfeaade5ed3159f85b81f9947a54a3bf64301
Minh Hiếu <gaconmvp2312@gmail.com>
2025-08-06 20:01:10 +0700

    init project

---
+++ src/test/java/com/cleanarchitecture/learncleanarchitecture/LearnCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanarchitecture.learncleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
